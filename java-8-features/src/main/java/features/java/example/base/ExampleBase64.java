74af7249969bcfa53f51b131c21f6ffbe5058041
OdirleiK <odirlei67777@gmail.com>
2024-03-09 09:31:53 -0300

    :coffee: java 8 features: Base64

---
+++ java-8-features/src/main/java/features/java/example/base/ExampleBase64.java
@@ -0,0 +1,25 @@
+package features.java.example.base;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class ExampleBase64 {
+
+	public static void main(String[] args) { 
+		
+	try {
+		String text = "The class base64 in java 8";
+		
+		String textEncoder = Base64.getEncoder().encodeToString(text.getBytes("utf-8"));
+		
+		String textDecoder = new String(Base64.getDecoder().decode(textEncoder), "utf-8");
+		
+		System.out.println(textEncoder);
+		System.out.println(textDecoder);
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+    
+	}
+}
